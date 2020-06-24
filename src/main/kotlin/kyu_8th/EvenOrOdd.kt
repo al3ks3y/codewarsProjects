2901b82c57a0e7cfc2fa7eb73eded4a8afda9bf8
alexeypetrov <lomon2008@yandex.ru>
2020-06-24 09:16:00 +0300

    EvenOrOdd task

---
+++ src/main/kotlin/kyu_8th/EvenOrOdd.kt
@@ -0,0 +1,13 @@
+package kyu_8th
+
+class Main {
+    fun evenOrOdd(number: Int): String {
+        if (number % 2 == 0) {
+            return "Even"
+        } else return "Odd"
+    }
+
+    fun main(args: Array<String>) {
+        print("")
+    }
+}
