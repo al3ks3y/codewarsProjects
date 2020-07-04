93ec25bf4694eab65aa83b4501f87c1f503a3fa1
alexeypetrov <lomon2008@yandex.ru>
2020-07-04 23:01:37 +0300

    Cycle task started

---
+++ src/main/kotlin/kyu_6th/1ncycle.kt
@@ -0,0 +1,41 @@
+package kyu_6th
+
+import java.math.BigDecimal
+
+class `1ncycle` {
+    companion object {
+        fun cycle(n: Int): Int {
+            val devided: String = BigDecimal.valueOf(1.0 / n).toString()
+            val charArray = devided.toCharArray().toMutableList()
+            repeat(2) { charArray.removeAt(0) }
+
+            for ((i, c) in charArray.withIndex()) {
+                if (i >= 2 && 3 * i - 2 < charArray.size &&
+                    !devided.substring(2, i).equals("") &&
+                    devided.substring(2, i).equals(devided.substring(i, i + devided.substring(2, i).length))
+                    && devided.substring(2, i).equals(
+                        devided.substring(
+                            i + devided.substring(2, i).length,
+                            i + 2 * (devided.substring(2, i).length)
+                        )
+                    )
+                    && devided.substring(2, i).equals(
+                        devided.substring(
+                            devided.length - devided.substring(2, i).length - 1,
+                            devided.length - 1
+                        )
+                    )
+                ) {
+                    println(devided.substring(2, i))
+                    return devided.substring(2, i).length
+                }
+            }
+            return -1
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(cycle(33))
+        }
+    }
+}
