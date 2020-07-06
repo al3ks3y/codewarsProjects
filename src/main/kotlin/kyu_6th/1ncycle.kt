705d4c9ce79342630f669e7a7809c0f49e7bf86f
alexeypetrov <lomon2008@yandex.ru>
2020-07-06 22:34:34 +0300

    Cycle task finished

--- src/main/kotlin/kyu_6th/1ncycle.kt
+++ src/main/kotlin/kyu_6th/1ncycle.kt
@@ -5,33 +5,17 @@ import java.math.BigDecimal
 class `1ncycle` {
     companion object {
         fun cycle(n: Int): Int {
-            val devided: String = BigDecimal.valueOf(1.0 / n).toString()
-            val charArray = devided.toCharArray().toMutableList()
-            repeat(2) { charArray.removeAt(0) }
-
-            for ((i, c) in charArray.withIndex()) {
-                if (i >= 2 && 2 * i - 2 < charArray.size &&
-                    !devided.substring(2, i).equals("") &&
-                    !devided.substring(2, i).equals("0") &&
-                    devided.substring(2, i).equals(devided.substring(i, i + devided.substring(2, i).length))
-                    && devided.substring(2, i).equals(
-                        devided.substring(
-                            i + devided.substring(2, i).length,
-                            i + 2 * (devided.substring(2, i).length)
-                        )
-                    )
-//                    && devided.substring(2, i).equals(
-//                        devided.substring(
-//                            devided.length - devided.substring(2, i).length - 1,
-//                            devided.length - 1
-//                        )
-//                    )
-                ) {
-                    //println(devided.substring(1+n.toString().length, i))
-                    return devided.substring(2, i).length
+            if (n % 2 == 0 || n % 5 == 0) {
+                return -1
+            } else {
+                var c = 1
+                var result = 10 % n
+                while (result % n != 1) {
+                    result = result * 10 % n
+                    c++
                 }
+                return c
             }
-            return -1
         }
 
         @JvmStatic
