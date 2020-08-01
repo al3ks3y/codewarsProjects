0e71931a4d69b9f66a5bb6364742217a1d1f1301
alexeypetrov <lomon2008@yandex.ru>
2020-08-01 18:47:41 +0300

    MultiplesOf3And5.kt added

---
+++ src/main/kotlin/kyu_6th/MultiplesOf3And5.kt
@@ -0,0 +1,12 @@
+package kyu_6th
+
+class MultiplesOf3And5 {
+        fun solution(number: Int): Int {
+            val set= mutableSetOf<Int>()
+            for (x in 3 until number){
+                if(x%3==0||x%5==0)set.add(x)
+            }
+            return set.stream().mapToInt {it!!}.sum()
+        }
+
+}
